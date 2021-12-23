01d49bd3674977755998c41abf0402c82402e9c2
suyeonhong <bellahong95@gmail.com>
2021-12-23 21:33:25 +0900

    logo image 적용

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,14 @@
 import { NavLink } from 'react-router-dom';
 
 function Header(){
+    const imgSrc = `${process.env.PUBLIC_URL}/img/logo.png`;
+
     return (
         <header>
             <div className='inner'>
-                <h1><NavLink exact activeStyle={{opacity: 1}} to="/">LOGO</NavLink></h1>
+                <h1><NavLink exact activeStyle={{opacity: 1}} to="/">
+                        <img src={imgSrc} alt="logo image" />
+                    </NavLink></h1>
                 <ul id="gnb">
                     <li><NavLink exact activeStyle={{opacity: 1}} to="Youtube">YOUTUBE</NavLink></li>
                     <li><NavLink exact activeStyle={{opacity: 1}} to="Flickr">FlICKR</NavLink></li>
