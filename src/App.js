11fab9e61cb62defbd7ce9b4b20a250d8671086e
suyeonhong <bellahong95@gmail.com>
2021-12-26 20:07:23 +0900

    최종수정

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import Youtube from './components/Youtube';
 import Gallery from './components/Gallery';
 import Join from './components/Join';
 import Footer from './components/Footer';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Header />
 
-      <Route exact path="/youtube" component={Youtube}></Route>
-      <Route exact path="/flickr" component={Gallery}></Route>
-      <Route exact path="/join" component={Join}></Route>
-      <Route exact path="/" component={Main}></Route>
+      <Switch>
+        <Route exact path="/portfolio_react/youtube" component={Youtube}></Route>
+        <Route exact path="/portfolio_react/flickr" component={Gallery}></Route>
+        <Route exact path="/portfolio_react/join" component={Join}></Route>
+        <Route exact path="/portfolio_react/" component={Main}></Route>
+      </Switch>
 
       <Footer />
     </>
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,15 @@ function Header(){
     return (
         <header>
             <div className='inner'>
-                <h1><NavLink exact activeStyle={{opacity: 1}} to="/">
+                <h1>
+                    <NavLink exact activeStyle={{opacity: 1}} to="/portfolio_react/">
                         <img src={imgSrc} alt="logo image" />
-                    </NavLink></h1>
+                    </NavLink>
+                </h1>
                 <ul id="gnb">
-                    <li><NavLink exact activeStyle={{opacity: 1}} to="/Youtube">YOUTUBE</NavLink></li>
-                    <li><NavLink exact activeStyle={{opacity: 1}} to="/Flickr">FLICKR</NavLink></li>
-                    <li><NavLink exact activeStyle={{opacity: 1}} to="/Join">JOIN</NavLink></li>
+                    <li><NavLink exact activeStyle={{opacity: 1}} to="/portfolio_react/Youtube">YOUTUBE</NavLink></li>
+                    <li><NavLink exact activeStyle={{opacity: 1}} to="/portfolio_react/Flickr">FLICKR</NavLink></li>
+                    <li><NavLink exact activeStyle={{opacity: 1}} to="/portfolio_react/Join">JOIN</NavLink></li>
                 </ul>
             </div>
         </header>
