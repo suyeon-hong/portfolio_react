9de06374b02dc29b1cbd2952bf81713f7768afe4
suyeonhong <bellahong95@gmail.com>
2021-12-26 16:47:31 +0900

    Youtube.js 오류 수정

--- src/components/Youtube.js
+++ src/components/Youtube.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-import { useEffect } from "react";
-import { useState } from "react/cjs/react.development";
+import { useEffect, useState } from "react";
 
 
 function Youtube(){
