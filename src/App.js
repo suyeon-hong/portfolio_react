53d5276770ff998f6d1129edd6fce19ae3dfceb2
suyeonhong <bellahong95@gmail.com>
2021-12-27 12:14:39 +0900

    switch 제거

--- src/App.js
+++ src/App.js
@@ -5,19 +5,17 @@ import Youtube from './components/Youtube';
 import Gallery from './components/Gallery';
 import Join from './components/Join';
 import Footer from './components/Footer';
-import { Route, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Header />
 
-      <Switch>
-        <Route exact path="/portfolio_react/youtube" component={Youtube}></Route>
-        <Route exact path="/portfolio_react/flickr" component={Gallery}></Route>
-        <Route exact path="/portfolio_react/join" component={Join}></Route>
-        <Route exact path="/portfolio_react/" component={Main}></Route>
-      </Switch>
+      <Route exact path="/portfolio_react/youtube" component={Youtube}></Route>
+      <Route exact path="/portfolio_react/flickr" component={Gallery}></Route>
+      <Route exact path="/portfolio_react/join" component={Join}></Route>
+      <Route exact path="/portfolio_react/" component={Main}></Route>
 
       <Footer />
     </>
